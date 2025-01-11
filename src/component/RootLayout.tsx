4a7645655d5e170dea83d4fa590250ebb9bc10ff
Kavindu <gayanthakavindu3@gmail.com>
2025-01-11 22:45:47 +0530

    created root lay out

---
+++ src/component/RootLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+import VerticalNav from "./VerticalNav";
+
+
+export function RootLayout() {
+  return (
+    <>
+      <VerticalNav />
+      <Outlet />
+      
+    </>
+  );
+}
