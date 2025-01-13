097b10f254c0155fdebb278df4bbd486540c07aa
Kavindu <gayanthakavindu3@gmail.com>
2025-01-13 10:04:59 +0530

    wrap provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from './store/Store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App/>
+     <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
