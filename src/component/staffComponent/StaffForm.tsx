bd0ce3ce0036e5853744a559da31472f9f6616c2
Kavindu <gayanthakavindu3@gmail.com>
2025-01-13 10:05:42 +0530

    created staf form component

---
+++ src/component/staffComponent/StaffForm.tsx
@@ -0,0 +1,189 @@
+import { useState } from "react";
+import Input from "antd/es/input";
+import Select from "antd/es/select";
+import DatePicker from "antd/es/date-picker";
+import { useDispatch } from "react-redux";
+import { addStaff } from "@/reducers/StaffSlice";
+
+const StaffForm = () => {
+  const dispatch = useDispatch();
+
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [gender, setGender] = useState("");
+  const [joindDate, setJoindDate] = useState(null);
+  const [dateOfBirth, setDateOfBirth] = useState(null);
+  const [buildingNo, setBuildingNo] = useState("");
+  const [lane, setLane] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+  const [fields, setFields] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
+  const [equipments, setEquipments] = useState([]);
+
+  const formData = {
+    firstName,
+    lastName,
+    designation,
+    gender,
+    joindDate,
+    dateOfBirth,
+    buildingNo,
+    lane,
+    state,
+    city,
+    postalCode,
+    contactNumber,
+    email,
+    role,
+    fields,
+    vehicles,
+    equipments,
+  };
+
+  //dispatch(addStaff(newStaff));
+
+  return (
+    <form>
+      {/* Personal Information */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Input
+          placeholder="Enter first name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input
+          placeholder="Enter last name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Select
+          placeholder="Select a designation"
+          options={[
+            { value: "ASSISTANTMANAGER", label: "Assistant Manager" },
+            { value: "ADMINANDHRSTAFF", label: "Admin and HR Staff" },
+            { value: "OFFICEASSISTANT", label: "Office Assistant" },
+          ]}
+          onChange={(value) => setDesignation(value)}
+        />
+        <Select
+          placeholder="Select gender"
+          options={[
+            { value: "MALE", label: "Male" },
+            { value: "FEMALE", label: "Female" },
+          ]}
+          onChange={(value) => setGender(value)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <DatePicker
+          placeholder="Enter join date"
+          onChange={(date) => setJoindDate(date)}
+        />
+        <DatePicker
+          placeholder="Enter date of birth"
+          onChange={(date) => setDateOfBirth(date)}
+        />
+      </div>
+
+      {/* Address Information */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Input
+          placeholder="Enter building no or name"
+          showCount
+          maxLength={30}
+          onChange={(e) => setBuildingNo(e.target.value)}
+        />
+        <Input
+          placeholder="Enter lane"
+          showCount
+          maxLength={30}
+          onChange={(e) => setLane(e.target.value)}
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Input
+          placeholder="Enter main city"
+          showCount
+          maxLength={30}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <Input
+          placeholder="Enter main state"
+          showCount
+          maxLength={30}
+          onChange={(e) => setState(e.target.value)}
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Input
+          placeholder="Enter postal code"
+          showCount
+          maxLength={30}
+          onChange={(e) => setPostalCode(e.target.value)}
+        />
+        <Input placeholder="Enter contact number" showCount maxLength={30} />
+      </div>
+
+      {/* Contact Information */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Input
+          placeholder="Enter contact number"
+          value={contactNumber}
+          onChange={(e) => setContactNumber(e.target.value)}
+        />
+        <Input
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      {/* Role and Assignments */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Select
+          placeholder="Select a role"
+          options={[
+            { value: "MANAGER", label: "Manager" },
+            { value: "TECHNICIAN", label: "Technician" },
+            { value: "OTHER", label: "Other" },
+          ]}
+          onChange={(value) => setRole(value)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Select
+          mode="multiple"
+          placeholder="Select fields"
+          onChange={(value) => setFields(value)}
+        />
+        <Select
+          mode="multiple"
+          placeholder="Select vehicles"
+          onChange={(value) => setVehicles(value)}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
+        <Select
+          mode="multiple"
+          placeholder="Select equipment"
+          onChange={(value) => setEquipments(value)}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default StaffForm;
