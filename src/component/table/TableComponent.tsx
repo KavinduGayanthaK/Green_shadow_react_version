f6e6bee28befb6ea04d88af78a18757c73f78429
Kavindu <gayanthakavindu3@gmail.com>
2025-01-16 19:02:51 +0530

    fix the table component

--- src/component/table/TableComponent.tsx
+++ src/component/table/TableComponent.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Table, TableColumnsType } from 'antd';
+import { Table } from 'antd';
+import type { TableColumnsType } from 'antd';
 import { createStyles } from 'antd-style';
+
 
 interface TableProps {
   dataSource:[]; 
@@ -8,22 +10,22 @@ interface TableProps {
 }
 
 const useStyle = createStyles(({ css, token }) => {
-    const { antCls } = token;
-    return {
-      customTable: css`
-        ${antCls}-table {
-          ${antCls}-table-container {
-            ${antCls}-table-body,
-            ${antCls}-table-content {
-              scrollbar-width: thin;
-              scrollbar-color: #eaeaea transparent;
-              scrollbar-gutter: stable;
-            }
+  const { antCls } = token;
+  return {
+    customTable: css`
+      ${antCls}-table {
+        ${antCls}-table-container {
+          ${antCls}-table-body,
+          ${antCls}-table-content {
+            scrollbar-width: thin;
+            scrollbar-color: #eaeaea transparent;
+            scrollbar-gutter: stable;
           }
         }
-      `,
-    };
-  });
+      }
+    `,
+  };
+});
 
   const TableComponent: React.FC<TableProps> = ({ dataSource, columns }) => { 
     const { styles } = useStyle();
