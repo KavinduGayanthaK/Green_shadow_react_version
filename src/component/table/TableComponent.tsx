fde8342b016702894f22c2282a4fa6d384968ea1
Kavindu <gayanthakavindu3@gmail.com>
2025-01-17 19:11:30 +0530

    refactor table component

--- src/component/table/TableComponent.tsx
+++ src/component/table/TableComponent.tsx
@@ -1,42 +1,40 @@
-import React from 'react';
-import { Table } from 'antd';
+import{ useState } from 'react';
+import { Table as AntTable } from 'antd';
 import type { TableColumnsType } from 'antd';
-import { createStyles } from 'antd-style';
 
-
-interface TableProps {
-  dataSource:[]; 
-  columns: TableColumnsType<any>;
+interface TableProps<T> {
+    columns: TableColumnsType<T>;
+    dataSource: T[];
 }
 
-const useStyle = createStyles(({ css, token }) => {
-  const { antCls } = token;
-  return {
-    customTable: css`
-      ${antCls}-table {
-        ${antCls}-table-container {
-          ${antCls}-table-body,
-          ${antCls}-table-content {
-            scrollbar-width: thin;
-            scrollbar-color: #eaeaea transparent;
-            scrollbar-gutter: stable;
-          }
-        }
-      }
-    `,
-  };
-});
+const Table = <T extends object>({ columns, dataSource }: TableProps<T>) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const pageSize = 5;
 
-  const TableComponent: React.FC<TableProps> = ({ dataSource, columns }) => { 
-    const { styles } = useStyle();
+    // Calculate data for the current page
+    const currentPageData = dataSource.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
+
     return (
-      <Table<TableProps>
-        className={styles.customTable}
-        columns={columns}
-        dataSource={dataSource}
-        scroll={{ x: 'max-content', y: 55 * 5 }}
-      />
+        <AntTable<T>
+            bordered
+            columns={columns}
+            dataSource={currentPageData}
+            pagination={{
+                pageSize, // Limit to 5 rows per page
+                current: currentPage,
+                total: dataSource.length, // Total rows in data
+                onChange: handlePageChange, // Page change handler
+                showSizeChanger: false, // Disable page size changer
+                style: { display: 'flex', justifyContent: 'center', alignItems: 'center' }, // Center the pagination
+            }}
+            key={currentPage} // Refresh table content when the page changes
+            scroll={{ x: 'max-content' }} // Enable horizontal scroll if table width exceeds
+        />
     );
-  };
+};
 
-export default TableComponent;
+export default Table;
