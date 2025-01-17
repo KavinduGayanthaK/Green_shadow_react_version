7575a88eed54f4b3b2cc6d114366f8a87e14d719
Kavindu <gayanthakavindu3@gmail.com>
2025-01-17 19:11:13 +0530

    created vehicle model

---
+++ src/models/VehicleModel.ts
@@ -0,0 +1,9 @@
+export class VehicleModel{
+    licensePlateNumber:string;
+    category:string;
+    fuelType:string;
+    vehicleStatus:string;
+    specialRemark:string;
+    vehicleStaffMember:string;
+
+}
