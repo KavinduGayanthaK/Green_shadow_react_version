70d2cbccc13b8ac1a88b1fca786a7b7cb3214237
Kavindu <gayanthakavindu3@gmail.com>
2025-01-19 20:18:18 +0530

    added to field reducer into store

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,4 +1,5 @@
 import EquipmentSlice from "@/reducers/EquipmentSlice";
+import FieldSlice from "@/reducers/FieldSlice";
 import StaffSlice from "@/reducers/StaffSlice";
 import VehicleSlice from "@/reducers/VehicleSlice";
 import { configureStore } from "@reduxjs/toolkit";
@@ -8,6 +9,7 @@ export const store = configureStore({
     reducer : {
         staff:StaffSlice,
         vehicle:VehicleSlice,
-        equipment:EquipmentSlice
+        equipment:EquipmentSlice,
+        field:FieldSlice
     }
 })
