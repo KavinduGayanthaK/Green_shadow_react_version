0a884cd64dfbcb4abd11c1ac33c0b9625140a63d
Kavindu <gayanthakavindu3@gmail.com>
2025-01-19 20:15:11 +0530

    created add,update and delete slice

---
+++ src/reducers/FieldSlice.ts
@@ -0,0 +1,32 @@
+import { FieldModel } from "@/models/FieldModel";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState : {field:FieldModel[]} = {field:[]}
+
+const FieldSlice = createSlice({
+    name:"field",
+    initialState,
+    reducers : {
+        // Add a equipment
+        addField: (state,action:PayloadAction<FieldModel>)=>{
+            state.field.push(action.payload);
+        },
+        updateField: (state, action:PayloadAction<FieldModel>)=>{
+            const index = state.field.findIndex(
+                (field) =>
+                    field.fieldCode = action.payload.fieldCode
+            );
+            if (index !== -1) {
+                state.field[index] = action.payload;
+            }
+        },
+        deleteField:(state,action:PayloadAction<{fieldCode:string}>) =>{
+            state.field = state.field.filter(
+                (field) => field.fieldCode != action.payload.fieldCode
+            );
+        }
+    }
+});
+
+export const { addField,updateField,deleteField} = FieldSlice.actions;
+export default FieldSlice.reducer;
