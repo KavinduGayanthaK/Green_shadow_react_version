eb32e39b31cab175c8891b0e53e0723b0952995c
Kavindu <gayanthakavindu3@gmail.com>
2025-02-13 12:10:09 +0530

    added log field into staff model

--- src/models/StaffModel.ts
+++ src/models/StaffModel.ts
@@ -1,42 +1,63 @@
 export class StaffModel {
-    staffId:string;
-    firstName:string;
-    lastName:string;
-    designation:string
-    gender:string;
-    joinedDate:string | null;
-    dateOfBirth:string | null;
-    buildingNumber:string;
-    lane:string;
-    city:string;
-    state:string;
-    postalCode:string;
-    contactNumber:string;
-    email:string;
-    role:string;
-    fields:string[];
-    vehicles:string[];
-    equipments:string[];
+  staffId: string;
+  firstName: string;
+  lastName: string;
+  designation: string;
+  gender: string;
+  joinedDate: string | null;
+  dateOfBirth: string | null;
+  buildingNumber: string;
+  lane: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  contactNumber: string;
+  email: string;
+  role: string;
+  fields: string[];
+  vehicles: string[];
+  equipments: string[];
+  logs: string[];
 
-    constructor(id:string,firstName:string,lastName:string,designation:string,gender:string,joinedDate:string | null,dateOfBirth:string | null,buildingNumber:string,lane:string,city:string,
-        state:string,postalCode:string,contactNumber:string,email:string,role:string,fields:string[],vehicles:string[],equipments:string[]) {
-        this.staffId = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.designation = designation;
-        this.gender = gender;
-        this.joinedDate = joinedDate;
-        this.dateOfBirth = dateOfBirth;
-        this.buildingNumber = buildingNumber;
-        this.lane = lane;
-        this.city = city;
-        this.state = state;
-        this.postalCode = postalCode;
-        this.contactNumber = contactNumber;
-        this.email = email;
-        this.role = role;
-        this.fields = fields;
-        this.vehicles = vehicles;
-        this.equipments = equipments;
-    }
+  constructor(
+    id: string,
+    firstName: string,
+    lastName: string,
+    designation: string,
+    gender: string,
+    joinedDate: string | null,
+    dateOfBirth: string | null,
+    buildingNumber: string,
+    lane: string,
+    city: string,
+    state: string,
+    postalCode: string,
+    contactNumber: string,
+    email: string,
+    role: string,
+    fields: string[],
+    vehicles: string[],
+    equipments: string[],
+    logs: string[]
+  ) {
+    this.staffId = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.designation = designation;
+    this.gender = gender;
+    this.joinedDate = joinedDate;
+    this.dateOfBirth = dateOfBirth;
+    this.buildingNumber = buildingNumber;
+    this.lane = lane;
+    this.city = city;
+    this.state = state;
+    this.postalCode = postalCode;
+    this.contactNumber = contactNumber;
+    this.email = email;
+    this.role = role;
+    this.fields = fields;
+    this.vehicles = vehicles;
+    this.equipments = equipments;
+    this.logs = logs;
+  }
 }
