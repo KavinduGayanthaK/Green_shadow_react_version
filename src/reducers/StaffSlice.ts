a4d6d5eecdfc00b31921dff12fb1a5d699783598
Kavindu <gayanthakavindu3@gmail.com>
2025-02-14 14:22:19 +0530

    created save,get,delete,update staff using axios and createAsuncThunk

--- src/reducers/StaffSlice.ts
+++ src/reducers/StaffSlice.ts
@@ -1,36 +1,104 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { StaffModel } from "@/models/StaffModel";
+import axios from "axios";
 
 // Initial state for the slice
 const initialState: { staff: StaffModel[] } = { staff: [] };
 
+const api = axios.create({
+  baseURL: "http://localhost:3000/staff",
+});
+
+export const saveStaff = createAsyncThunk(
+  'staff/addStaff',
+  async (staff:StaffModel) =>{
+    try {
+      const response = await api.post('/add',staff);
+      return response.data;
+    }catch(error) {
+      return error;
+    }
+  }
+);
+
+export const getStaff = createAsyncThunk(
+  'staff/getAllStaff',
+  async () =>{
+    const response = await api.get('/getAllStaff');
+    return response.data;
+  }
+)
+
+export const updateStaff = createAsyncThunk(
+  'staff/updateStaff',
+    async (staff:StaffModel)=>{
+        try{
+            const response = await api.put(`/update/${staff.staffId}`,staff);
+            return response.data;
+        }catch (error){
+            return error;
+        }
+    }
+)
+
+export const deleteStaff = createAsyncThunk(
+  'stafff/deleteStaff',
+  async (staffId:string)=>{
+      try{
+          const response = await api.delete(`/delete/${staffId}`);
+          return response.data;
+      }catch (error){
+          return error;
+      }
+  }
+);
+
+
 const StaffSlice = createSlice({
   name: "staff",
   initialState,
-  reducers: {
-    // Add a vehicle
-    addStaff: (state, action: PayloadAction<StaffModel>) => {
-      state.staff.push(action.payload);
-    },
-    // Update a vehicle
-    updateStaff: (state, action: PayloadAction<StaffModel>) => {
-      const index = state.staff.findIndex(
-        (staff) =>
-          staff.staffId === action.payload.staffId
-      );
-      if (index !== -1) {
-        state.staff[index] = action.payload;
-      }
-    },
-    //Delete vehicle
-    deleteStaff: (state, action: PayloadAction<{ staffId: string }>) => {
-        state.staff = state.staff.filter(
-          (staff) => staff.staffId !== action.payload.staffId
-        );
-      },
+  reducers:{},
+  extraReducers: builder=> {
+    builder
+      .addCase(saveStaff.fulfilled, (state, action) =>{
+        state.staff.push(action.payload)
+      })
+      .addCase(saveStaff.pending, (state, action) =>{
+        console.log(action.payload);
+      })
+      .addCase(saveStaff.rejected, (state, action) =>{
+        console.log("Failed to save staff:",action.payload);
+      })
+      .addCase(getStaff.fulfilled, (state, action) => {
+        state.staff = action.payload; // Ensure the fetched array replaces the state
+      })
+      .addCase(getStaff.pending, (state, action) =>{
+        console.log(action.payload);
+      })
+      .addCase(getStaff.rejected, (state, action) =>{
+        console.log("Failed to get staff:",action.payload);
+      })
+      .addCase(updateStaff.fulfilled, (state, action) =>{
+        state.staff.push(action.payload)
+      })
+      .addCase(updateStaff.pending, (state, action) =>{
+        console.log(action.payload);
+      })
+      .addCase(updateStaff.rejected, (state, action) =>{
+        console.log("Failed to update staff:",action.payload);
+      })
+      .addCase(deleteStaff.fulfilled, (state, action) =>{
+        state.staff.push(action.payload)
+      })
+      .addCase(deleteStaff.pending, (state, action) =>{
+        console.log(action.payload);
+      })
+      .addCase(deleteStaff.rejected, (state, action) =>{
+        console.log("Failed to delete staff:",action.payload);
+      })
       
-  },
+  }
 });
 
-export const { addStaff, updateStaff,deleteStaff } = StaffSlice.actions;
+
 export default StaffSlice.reducer;
